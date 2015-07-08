2e71e38d12ae42bebf62925c8fd719acb8b370a2
Ahmad Shah Hafizan Hamidin <ahmadshahhafizan@gmail.com>
2015-07-08 14:11:35 +0800

    added servcie provider
    Signed-off-by: Ahmad Shah Hafizan Hamidin <ahmadshahhafizan@gmail.com>

---
+++ src/Minion.php
@@ -0,0 +1,40 @@
+<?php namespace Fv\Minions;
+
+use Artisaninweb\SoapWrapper\Wrapper as SoapWrapper;
+
+class Minion extends SoapWrapper
+{
+    protected $settings = [];
+
+    public function __construct(array $settings)
+    {
+        $this->settings = $settings;
+    }
+
+    public function createSoapInstance()
+    {
+        $this->add(function ($soap) {
+            $settings = $this->getSettings();
+
+            $soap->name('minions')
+                ->wsdl($settings['wsdl'])
+                ->cache(WSDL_CACHE_NONE)
+                ->options([
+                    'username' => $settings['username'],
+                    'password' => $settings['password']
+                ]);
+        });
+
+        return $this;
+    }
+
+    public function getSettings()
+    {
+        return $this->settings;
+    }
+
+    public function getMinionInstance()
+    {
+        return $this->services()['minions'];
+    }
+}
--- src/MinionServiceProvider.php
+++ src/MinionServiceProvider.php
@@ -4,8 +4,47 @@ use Illuminate\Support\ServiceProvider;
 
 class MinionServiceProvider extends ServiceProvider
 {
+    protected $workers = [
+        'fv.minion.order' => Fv\Minions\Workers\Order::class
+        'fv.minion.shipment' => Fv\Minions\Workers\Shipment::class
+    ];
+
     public function register()
     {
-        
+        $this->registerSoap();
+        $this->registerWorkers();
+    }
+
+    public function provides()
+    {
+        return ['fv.minions'];
+    }
+
+    protected function getWorkers()
+    {
+        return $workers;
+    }
+
+    protected function registerSoap()
+    {
+        $this->app->bindShared('fv.minions', function ($app) {
+            $settings = $app['config']->get('services.minion');
+
+            $minion = new Minion($settings);
+            $soap = $minion->createSoapInstance()->getMinionInstance();
+
+            return $soap;
+        });
+    }
+
+    protected function registerWorkers()
+    {
+        $worker = $this->getWorkers();
+
+        foreach ($workers as $name => $class) {
+            $this->app->bindShared($name, function ($app) {
+                return new $class($app['fv.minions']);
+            });
+        }
     }
 }
