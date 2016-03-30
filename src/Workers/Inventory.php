db788b944f211a725406575ec4fa9d53066ddb8d
Azri Ismail <azri@fashionvalet.com>
2016-03-30 10:45:44 +0800

    update inventory

--- src/Workers/Inventory.php
+++ src/Workers/Inventory.php
@@ -9,22 +9,31 @@ use Fv\Minions\Contracts\Worker\InventoryInterface;
  * Date Created : Feb 16, 2016 11:30:51 AM
  * File         : Inventory.php
  * Copyright    : rifkiyandhi@gmail.com
- * Function     : 
+ * Function     :
  */
 class Inventory extends Worker implements InventoryInterface
 {
 
+    /**
+     * @param array $product_ids Array of product id or sku
+     * @return mixed
+     */
     public function stockList(array $product_ids)
     {
         return $this->execute('catalogInventoryStockItemList', $product_ids);
     }
 
+    /**
+     * @param $product_id Product id or sku
+     * @param $data
+     * @return mixed
+     */
     public function stockUpdate($product_id, $data)
     {
         return $this->getSoapService()->call('catalogInventoryStockItemUpdate', [
-                    $this->getSoapSession(),
-                    $product_id,
-                    $data
+            $this->getSoapSession(),
+            $product_id,
+            $data
         ]);
     }
 
