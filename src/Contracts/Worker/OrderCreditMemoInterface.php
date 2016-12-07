3442ffd2717a0e159bc6f738eff4f2a99c929d7a
Rifki Yandhi <rifkiyandhi@gmail.com>
2016-12-07 17:46:26 +0800

    add credit memo rest

--- src/Contracts/Worker/OrderCreditMemoInterface.php
+++ src/Contracts/Worker/OrderCreditMemoInterface.php
@@ -9,13 +9,11 @@ interface OrderCreditMemoInterface
 
     public function getOrderCreditMemo($id);
 
-    public function createOrderCreditMemo($order_increment_id, array $data,
-                                          $comment = '', $isNotify = false,
-                                          $is_include_comment_in_email = false);
+    public function createOrderCreditMemo(array $data);
 
     public function addOrderCreditMemoComment($id, $comment = '',
                                               $isNotify = false,
-                                              $is_include_comment_in_email = false);
+                                              $isVisibleFrontend = false);
 
     public function cancelOrderCreditMemo($id);
 }
